d2203f0958f6777400fcfb1e5b338a35dc5eecee
Micha Surber <msurber@futurelab.ch>
2015-10-28 17:35:04 +0100

    intitial import of the digitalSTROM dizmo structure

---
+++ digitalSTROM/src/application.js
@@ -0,0 +1,13 @@
+// If your dizmo has a back side, include this function. Otherwise you
+// can delete it!
+function showBack() {
+    dizmo.showBack();
+}
+
+// As soon as the dom is loaded, and the dizmo is ready, instantiate the main class
+window.document.addEventListener('dizmoready', function() {
+    // Your code should be in here so that it is secured that the dizmo is fully loaded
+    document.getElementById('doneBtn').onclick = function() {
+        dizmo.showFront();
+    };
+});
