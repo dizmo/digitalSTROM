ea7830f04035cb93d948e48f71d3623593e8d227
Micha Surber <msurber@futurelab.ch>
2015-10-29 10:15:10 +0100

    moved js files to new directory
    changed the application js to have the new desired content
    moved the js files to the directory where they can be found
    requires not working yet

--- digitalSTROM/src/application.js
+++ digitalSTROM/src/application.js
@@ -1,3 +1,6 @@
+//= require flab/Namespace
+//= require flab/controller/genesis
+
 // If your dizmo has a back side, include this function. Otherwise you
 // can delete it!
 function showBack() {
@@ -7,7 +10,23 @@ function showBack() {
 // As soon as the dom is loaded, and the dizmo is ready, instantiate the main class
 window.document.addEventListener('dizmoready', function() {
     // Your code should be in here so that it is secured that the dizmo is fully loaded
-    document.getElementById('doneBtn').onclick = function() {
-        dizmo.showFront();
-    };
+	var genesis = new flab.controller.genesis(jQuery("#frontcontent") , jQuery("#backcontent"));
+});
+
+
+function showFront(event) {
+    dizmo.showFront();
+}
+
+dizmo.onShowBack(function() {
+    jQuery(".front").hide();
+    dizmo.setSize(256,306);
+    jQuery(".back").show();
+});
+
+dizmo.onShowFront(function() {
+    console.log('test 4');
+    jQuery(".back").hide();
+    dizmo.setSize(512,266);
+    jQuery(".front").show();
 });
