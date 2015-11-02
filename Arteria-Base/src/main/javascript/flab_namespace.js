c1b0da6fec731ed39d05a896e5738e00491441c9
Micha Surber <msurber@futurelab.ch>
2015-11-02 08:32:36 +0100

    removed futureLAB specific unused code

--- Arteria-Base/src/main/javascript/flab_namespace.js
+++
@@ -1,7 +0,0 @@
-//= compat
-//= require <flab/Namespace>
-/**
- * @namespace Special namespace for futureLAB Javascript classes.
- * @name flab
- */
-flab.Namespace.create('flab');
--- digitalSTROM-Genesis/src/test/fixtures/json/event/get.cgi
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/perl
-sleep 10;
-print "Content-type: application/json\r\n\r\n";
-print "{\"ok\":true}";
